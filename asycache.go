4cf5b2f72cb144a97d8c72cb4b34e1d54e52fdfa
Alexander Katasonov <alexander.katasonov@gmail.com>
2014-06-26 12:03:46 +0200

    Implemented cleanup for elements

--- asycache.go
+++ asycache.go
@@ -6,24 +6,27 @@ import (
 
 type cacheEntity struct {
 	data interface {}
+	life_time Duration
 	last_updated Time
 }
 
-type setChanParam struct{k string; v interface{}}
+type setChanParam struct{k string; v interface{}; finish_chan chan bool;life_time Duration}
 type getChanParam struct{k string; clbk chan <- interface{};}
 
 type Cache struct {
 	entities map[string]*cacheEntity
 	set_chan chan setChanParam
 	get_chan chan getChanParam
+	//revision_dt Duration //time delta for doing revision of Cache (e.g. removing outdated elements)
 }
 
-func MakeCache()(*Cache) {
+func MakeCache(cleanup_dt Duration)(*Cache) {
 	c := &Cache{entities: make(map[string]*cacheEntity),
 		set_chan: make(chan setChanParam, 100),
 		get_chan: make(chan getChanParam, 100)}
 
 	go func (){
+		ticker := NewTicker(cleanup_dt)
 		for {
 			select {
 			case gcp := <- c.get_chan:
@@ -34,7 +37,15 @@ func MakeCache()(*Cache) {
 				}
 				gcp.clbk <- v.data
 			case scp := <- c.set_chan:
-				c.entities[scp.k] = &cacheEntity{data: scp.v, last_updated: Now()}
+				c.entities[scp.k] = &cacheEntity{data: scp.v, life_time: scp.life_time, last_updated: Now()}
+				scp.finish_chan <- true
+			case <- ticker.C:
+				for k, v := range c.entities {
+					//remove element if it was not updated longer than it's life time
+					if Now().Sub(v.last_updated) >= v.life_time {
+						delete(c.entities, k)
+					}
+				}
 			}
 		}
 	}()
@@ -42,9 +53,13 @@ func MakeCache()(*Cache) {
 	return c
 }
 
-func (c *Cache) Set(k string, v interface{}) {
-	c.set_chan <- setChanParam{k, v}
-	return
+//k is a key
+//v is a value of any type - could be also a pointer
+//dt - life time for the element
+func (c *Cache) Set(k string, v interface{}, dt Duration) chan bool {
+	finish_chan := make(chan bool)
+	c.set_chan <- setChanParam{k, v, finish_chan, dt}
+	return finish_chan
 }
 
 func (c *Cache) Get(k string, timeout Duration) (interface{}, bool) {
@@ -66,3 +81,4 @@ func (c *Cache) Get(k string, timeout Duration) (interface{}, bool) {
 	}
 	return v, false
 }
+
--- asycache_test.go
+++ asycache_test.go
@@ -6,15 +6,59 @@ import (
 )
 
 func TestICanAddAndGet(t *testing.T) {
-	c := MakeCache()
-	c.Set("a", "hello")
-	c.Set("b", "world")
-	v, ok := c.Get("a", 1*time.Second)
-	var s string
-	if ok {
-		s = v.(string)
+	c := MakeCache(1*time.Minute)
+	data := map[string]string{"k1": "v1", "k2": "v2", "k3": "v3", "k4": "v4", "k5": "v5"}
+	fchans := make([] chan bool, len(data))
+	i := 0
+	for k, v := range data {
+		fchans[i] = c.Set(k, v, 1*time.Minute)
+		i++
 	}
-	if s != "hello" {
-		t.Error("Cant get value")
+	//waiting for setting operation be accomplished
+	for i = 0; i < len(fchans); i++ {
+		<- fchans[i]
+	}
+	for k, v := range data {
+		cv, ok := c.Get(k, 1*time.Second)
+		var s string
+		if ok {
+			s = cv.(string)
+		}
+		if s != v {
+			t.Errorf("Map value %v != Cache value %v with key %v", v, s, k)
+		}
+	}
+}
+
+func TestICanAddAndReplaceElement(t *testing.T) {
+	c := MakeCache(1*time.Minute)
+	<- c.Set("a", "hello", 1*time.Minute)
+	<- c.Set("a", "world", 1*time.Minute)
+	v, ok := c.Get("a", 1*time.Second)
+	if !ok {
+		t.Error("Element not found")
+		return
+	}
+	s := v.(string)
+	if s != "world" {
+		t.Errorf("string has value = %v, but should world", s)
+	}
+}
+
+//test requires some time about 5 seconds to execute
+func TestThatOutdatedItemsWillBeRemovedInTime(t *testing.T) {
+	c := MakeCache(1*time.Second)
+	<- c.Set("a", "hello", 3*time.Second)
+	<- c.Set("b", "world", 10*time.Second)
+	time.Sleep(5*time.Second)
+	_, ok := c.Get("a", 1*time.Second)
+	if ok {
+		t.Error("Element should be deleted")
+		return
+	}
+	_, ok = c.Get("b", 1*time.Second)
+	if !ok {
+		t.Error("Element should not be deleted")
+		return
 	}
 }
